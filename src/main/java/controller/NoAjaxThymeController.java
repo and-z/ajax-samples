b2564a95af6257246f1e372dd38e028c98102697
Andreas Zimpel <mail4andz@gmail.com>
2012-09-18 17:58:53 +0200

    + added noajax controller returning first user form view

---
+++ src/main/java/controller/NoAjaxThymeController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+
+@Controller
+public class NoAjaxThymeController {
+	
+	@RequestMapping(value="/userNoAjaxThyme",method=RequestMethod.GET)
+	public String showFormNoAjax(Model model){
+		model.addAttribute("user", new User());
+		return "pages/01-userForm";
+	}
+	
+	@RequestMapping(value="/userNoAjaxThyme",method=RequestMethod.POST)
+	public String processFormNoAjax(Model model, @Valid User user, BindingResult result ){
+		if(result.hasErrors()) {
+			return "01-no-ajax/userForm";
+		} 
+		else {
+			return "success";
+		}
+	}
+	
+}
