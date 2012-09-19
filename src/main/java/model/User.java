b6c23595a38936f15c3ec1d6e1b8645f8beba65c
Andreas Zimpel <mail4andz@gmail.com>
2012-09-19 15:58:10 +0200

    + added second firstName validation rule

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,30 +2,30 @@ package model;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.Size;
+
 public class User {
-	
-	@NotEmpty
-	private String firstName;
-	
-	@NotEmpty
-	private String lastName;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    @NotEmpty
+    @Size(min = 3, max = 20)
+    private String firstName;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    @NotEmpty
+    private String lastName;
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	
-	
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
