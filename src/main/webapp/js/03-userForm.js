e01d4122686dc25d154bba308d5fb2bc8d28387c
Andreas Zimpel <mail4andz@gmail.com>
2012-09-19 16:41:10 +0200

    + added 3.th example template including bootstrap css

---
+++ src/main/webapp/js/03-userForm.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    var collectFormData = function(fields){
+        "use strict";
+
+        var data = {};
+        for (var i = 0; i < fields.length; i++) {
+            var $item = $(fields[i]);
+            data[$item.attr('name')] = $item.val();
+        }
+        return data;
+    }
+
+    var formSubmitHandler = function(response){
+        "use strict";
+
+        $form.find('.control-group').removeClass('error');
+        $form.find('.help-inline').empty();
+        $form.find('.alert').remove();
+
+        if (response.status == 'FAIL') {
+            for (var i = 0; i < response.errorMessageList.length; i++) {
+                var item = response.errorMessageList[i];
+                var $controlGroup = $('#' + item.fieldName + 'ControlGroup');
+                $controlGroup.addClass('error');
+                $controlGroup.find('.help-inline').html(item.message);
+            }
+        } else {
+            $form.unbind('submit');
+            $form.submit();
+        }
+    }
+
+    var $form = $('#add-user-form');
+    $form.bind('submit', function(e) {
+        // Ajax validation
+        var $inputs = $form.find('input');
+        var data = collectFormData($inputs);
+
+        $.post('userAjaxBootstrapThyme.json',
+            data,
+            formSubmitHandler,
+            'json'
+        );
+
+        e.preventDefault();
+        return false;
+    });
+});
